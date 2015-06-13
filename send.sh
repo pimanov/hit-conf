be74f0ab03a4e4b7d659fe921058e4c334492a3a
Vladimir Pimanov <pimanov.vladimir@gmail.com>
2015-06-13 08:55:06 +0300

    is ok to send to /2014/

--- send.sh
+++ send.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-scp -P 23 -r _site/* pimanov@users.imec.msu.ru:/home/pimanov/hit-conf/www/
+
+rm -rf _site
+jekyll build
+
+for file in _site/*.html
+do
+    sed 's/charset=UTF-8/charset=cp-1251/g' $file > tim
+    mv tim $file
+done
+
+for file in _site/*.*
+do
+    iconv -f utf-8 -t windows-1251 -o tim $file
+    mv tim $file
+done
+
+scp -P 23 -r _site/* pimanov@users.imec.msu.ru:/home/pimanov/hit-conf/www/2014/
+
+#rm -rf _site
