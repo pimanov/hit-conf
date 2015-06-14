f4132735decebdbd3fba45756eb46016226dc843
Vladimir Pimanov <pimanov.vladimir@gmail.com>
2015-06-14 14:08:30 +0300

    -books.html, links to books are ok, send encoding are ok

--- send.sh
+++ send.sh
@@ -4,14 +4,16 @@ rm -r _site
  
 jekyll build
 
-for file in _site/*.html
+for file in _site/*.html _site/2016/*.html
 do
+    echo html file charset modification: $file
     sed 's/charset=UTF-8/charset=cp-1251/g' $file > tim
     mv tim $file
 done
 
-for file in _site/*.*
+for file in _site/*.* _site/2016/*.*
 do
+    echo recode $file
     iconv -f utf-8 -t windows-1251 -o tim $file
     mv tim $file
 done
